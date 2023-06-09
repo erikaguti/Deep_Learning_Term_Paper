7bba691010a75d6a0ba18b7e0d6bb7f437eac3e2
yzse <54381977+yzse@users.noreply.github.com>
2023-06-09 22:48:17 +0200

    add svhn

--- Models/denoiser_model.py
+++ Models/denoiser_model.py
@@ -109,3 +109,102 @@ class ConvDenoiser():
         predictions = autoencoder.predict(X_noisy)
 
         return history, score, predictions
+    
+ class CNN():
+    def __init__(self, X, X_test, y, y_test):
+        self.X = X
+        self.X_test = X_test
+        self.y = y
+        self.y_test = y_test
+
+    def model(self, X, X_test, y, y_test):
+        X = np.array(X)
+        X_test = np.array(X_test)
+        y = np.array(y)
+        y_test = np.array(y_test)
+
+        X = X.reshape((-1, 32, 32, 3))
+        X_test = X_test.reshape((-1, 32, 32, 3))
+
+         # normalize pixel values to the range [0, 1]
+        X = X / 255.0
+        X_test = X_test / 255.0
+
+        # network params
+        input_shape = (32, 32, 3)
+        batch_size = 64
+        validation_split = 0.25
+        epochs = 100
+        kernel_size = 3
+        latent_dim = 64
+        layer_filters = [16, 32]
+
+        # encoder
+        inputs = Input(shape=input_shape, name='encoder_input')
+        x = inputs
+
+        # Stack of Conv2D(64)-Conv2D(128)
+        for filters in layer_filters:
+            x = Conv2D(filters=filters,
+                       kernel_size=kernel_size,
+                       strides=2,
+                       activation='relu',
+                       padding='same')(x)
+
+        shape = K.int_shape(x)
+
+        # latent vector
+        x = Flatten()(x)
+        latent = Dense(latent_dim, name='latent_vector')(x)
+
+        # encoder
+        encoder = Model(inputs, latent, name='encoder')
+        encoder.summary()
+
+        # decoder
+        latent_inputs = Input(shape=(latent_dim,), name='decoder_input')
+        x = Dense(shape[1] * shape[2] * shape[3])(latent_inputs)
+        x = Reshape((shape[1], shape[2], shape[3]))(x)
+
+        # dtack of Conv2DTranspose(64)-Conv2DTranspose(32)
+        for filters in layer_filters[::-1]:
+            x = Conv2DTranspose(filters=filters,
+                                kernel_size=kernel_size,
+                                strides=2,
+                                activation='relu',
+                                padding='same')(x)
+
+        outputs = Conv2DTranspose(filters=3,
+                                  kernel_size=kernel_size,
+                                  padding='same',
+                                  activation='sigmoid',
+                                  name='decoder_output')(x)
+
+        # instantiate decoder
+        decoder = Model(latent_inputs, outputs, name='decoder')
+        decoder.summary()
+
+        # instantiate autoencoder
+        model = Model(inputs, decoder(encoder(inputs)), name='autoencoder')
+        model.summary()
+
+        model.compile(optimizer='adam', loss='binary_crossentropy')
+        model.summary()
+
+        # train model
+        history = model.fit(X,
+                    X_test,
+                    validation_split=validation_split,
+                    epochs=epochs,
+                    batch_size=batch_size)
+        
+        # score
+        score = model.evaluate(X, X_test)
+
+        model.save('cnn_model.h5')
+
+        # predict autoencoder output
+        predictions = model.predict(X_test)
+
+        return history, score, predictions
+
